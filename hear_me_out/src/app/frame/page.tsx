c7155342aca68ca92cd9492a3bb3e9cc04d3ae1d
Romario Kavin <98270892+RomarioKavin1@users.noreply.github.com>
2024-03-23 15:14:01 +0530

    privy

--- hear_me_out/src/app/frame/page.tsx
+++ hear_me_out/src/app/frame/page.tsx
@@ -1,51 +1,49 @@
 "use client";
 import { getFrameMetadata } from "@coinbase/onchainkit/frame";
 import type { Metadata } from "next";
-import { NEXT_PUBLIC_URL } from "./config";
+import { NEXT_PUBLIC_URL } from "../config";
 import Link from "next/link";
 import { usePrivy } from "@privy-io/react-auth";
-import { useEffect } from "react";
-import { redirect } from "next/navigation";
 
-// const frameMetadata = getFrameMetadata({
-//   buttons: [
-//     {
-//       label: "What Am I listening to ?",
-//       action: "post",
-//       target: `${NEXT_PUBLIC_URL}/nowlistening`,
-//     },
-//     {
-//       label: "My stats",
-//     },
-//     {
-//       label: "Get my playlist",
-//     },
-//     {
-//       label: "Suggest songs",
-//     },
-//   ],
-//   image: {
-//     src: `${NEXT_PUBLIC_URL}/hearmeout.png`,
-//     aspectRatio: "1.91:1",
-//   },
-//   input: {
-//     text: "Tell me a story",
-//   },
-//   postUrl: `${NEXT_PUBLIC_URL}/api/frame`,
-// });
+const frameMetadata = getFrameMetadata({
+  buttons: [
+    {
+      label: "What Am I listening to ?",
+      action: "post",
+      target: `${NEXT_PUBLIC_URL}/nowlistening`,
+    },
+    {
+      label: "My stats",
+    },
+    {
+      label: "Get my playlist",
+    },
+    {
+      label: "Suggest songs",
+    },
+  ],
+  image: {
+    src: `${NEXT_PUBLIC_URL}/hearmeout.png`,
+    aspectRatio: "1.91:1",
+  },
+  input: {
+    text: "Tell me a story",
+  },
+  postUrl: `${NEXT_PUBLIC_URL}/api/frame`,
+});
 
-// export const metadata: Metadata = {
-//   title: "zizzamia.xyz",
-//   description: "LFG",
-//   openGraph: {
-//     title: "zizzamia.xyz",
-//     description: "LFG",
-//     images: [`${NEXT_PUBLIC_URL}/park-1.png`],
-//   },
-//   other: {
-//     ...frameMetadata,
-//   },
-// };
+export const metadata: Metadata = {
+  title: "zizzamia.xyz",
+  description: "LFG",
+  openGraph: {
+    title: "zizzamia.xyz",
+    description: "LFG",
+    images: [`${NEXT_PUBLIC_URL}/park-1.png`],
+  },
+  other: {
+    ...frameMetadata,
+  },
+};
 
 export default function Page() {
   // const router = useRouter();
@@ -63,11 +61,6 @@ export default function Page() {
   //     console.error('Error:', error);
   //   }
   // };
-  useEffect(() => {
-    if (authenticated) {
-      redirect("/home");
-    }
-  }, []);
   const { ready, authenticated, login } = usePrivy();
   const disableLogin = !ready || (ready && authenticated);
 
